f4eba96a92002053be67064079f13d007491b722
Egil Hansen <egil@assimilated.dk>
2021-04-10 21:50:12 +0000

    versioning

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace workflows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
