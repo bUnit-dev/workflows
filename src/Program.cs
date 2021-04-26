8a340089156f8bff56e43ed09ab455928bd25543
Egil Hansen <egil@assimilated.dk>
2021-04-26 10:39:20 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace workflows
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); adsf asdf asdf
+            Console.WriteLine("Hello World!");
         }
     }
 }
