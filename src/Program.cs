1e62e3758df35f3f6262f00d00df5a3d34c5c29e
Egil Hansen <egil@assimilated.dk>
2021-04-26 14:48:03 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace workflows
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from Twitch!");
         }
     }
 }
